3a7cb714d64d8965a04b1dd0178ed0a39a7d0bc4
paul stumpe <paul.andrew.stumpe@gmail.com>
2024-07-22 07:49:49 -0500

    updates to styling

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+   $('#carouselWidgets').css('height');
+    // now apply this height as a max-height on all the image items; css will handle the rest;
+    $('#carouselWidgets').find('.carousel-item img').css('max-height', $('#carouselwithIndicators').css('height'))
+});
